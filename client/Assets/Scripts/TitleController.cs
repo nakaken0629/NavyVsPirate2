3d66da5504faa8b30f8bb7bf39872ce2c1ba2b5a
NAKAGAKI Kenji <nakaken0629@gmail.com>
2018-01-24 00:52:32 +0900

    GameStartボタンのクリックイベントを作成

---
+++ client/Assets/Scripts/TitleController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+	public void OnClickGameStart() {
+		Debug.Log ("click gamestart");
+	}
+}
